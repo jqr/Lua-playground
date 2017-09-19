3f1586b29f2b3d98dcd414a2844abac3c8c2d5eb
Elijah Miller <elijah.miller@gmail.com>
2017-09-18 20:53:28 -0400

    Add an example of calling Lua from Ruby.

---
+++ from_ruby/rlua.rb
@@ -0,0 +1,31 @@
+# brew install lua@5.1
+# gem install rlua
+require 'rlua'
+
+state = Lua::State.new
+
+# We probably woudln't use anything from stdlib
+# state.__load_stdlib :all
+
+state.input = 10                     # set a value
+state.__eval "output = input * 3"    # execute!
+puts "state.output: #{state.output}" # retrieve a value
+
+result = state.__eval "return 'hi'"   # or just return a value
+puts "return: #{result}"
+
+puts
+
+# share Ruby code with lua
+state.print = method(:puts) # anything that supports call
+state.__eval "print('hello from lua')"
+
+puts
+
+# share Lua code with Ruby
+state.__eval "
+function double(value)
+  return value * 2
+end
+"
+puts "double(30): #{state.double(30)}"
